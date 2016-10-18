1e7d488ef568d8c331ff473913f4986b678f1e9f
Administrador <Administrador@jarvis>
2016-10-17 22:34:03 -0300

    Finalização da tela de vinculação de beans

--- src/adriano/b/cadastrousuario/view/MenuView.java
+++ src/adriano/b/cadastrousuario/view/MenuView.java
@@ -43,6 +43,8 @@ public class MenuView extends javax.swing.JFrame {
         jMenuItem1 = new javax.swing.JMenuItem();
         itemProduto = new javax.swing.JMenuItem();
         jMenuItem2 = new javax.swing.JMenuItem();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem3 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Cadastro de Usuários");
@@ -104,6 +106,18 @@ public class MenuView extends javax.swing.JFrame {
 
         menuBar.add(fileMenu);
 
+        jMenu1.setText("Sistema");
+
+        jMenuItem3.setText("Serviços do sistema");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem3);
+
+        menuBar.add(jMenu1);
+
         setJMenuBar(menuBar);
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(getContentPane());
@@ -162,6 +176,13 @@ public class MenuView extends javax.swing.JFrame {
         this.centralizaForm(cv);
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        ServicoDoSistemaView sv = new ServicoDoSistemaView();
+        this.desktopPane.add(sv);
+        sv.setVisible(true);
+        this.centralizaForm(sv);
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -203,8 +224,10 @@ public class MenuView extends javax.swing.JFrame {
     private javax.swing.JMenuItem itemAbreGrupo;
     private javax.swing.JMenuItem itemMarcaOpen;
     private javax.swing.JMenuItem itemProduto;
+    private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JMenuBar menuBar;
     private javax.swing.JMenuItem openMenuItem;
     // End of variables declaration//GEN-END:variables
